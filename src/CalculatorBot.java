5edcff0e7a376f5f2ea4221d476d088faf172502
Moein Arabi <moein.mo81@gmail.com>
2022-02-28 18:01:06 +0330

    Added calculation method

--- src/CalculatorBot.java
+++ src/CalculatorBot.java
@@ -5,6 +5,8 @@ import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMar
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
+import java.math.BigDecimal;
+import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,17 +15,21 @@ public class CalculatorBot extends TelegramLongPollingBot {
     @Override
     public void onUpdateReceived(Update update) {
         if (update.hasMessage() && update.getMessage().hasText()) {
+            String text = update.getMessage().getText();
+
             System.out.println(update.getMessage().getChat().toString());
-            System.out.println(update.getMessage().getText());
+            System.out.println(text);
+
+            if (text.charAt(0) >= '0' || text.charAt(0) <= '9')
+                sendText(calculate(text), update.getMessage().getChatId().toString());
 
             sendCustomKeyboard(update.getMessage().getChatId().toString());
         }
     }
 
-    public void sendCustomKeyboard(String chatId) {
+    private void sendCustomKeyboard(String chatId) {
         SendMessage message = new SendMessage();
         message.setChatId(chatId);
-        message.setText("Custom message text");
 
         ReplyKeyboardMarkup keyboardMarkup = new ReplyKeyboardMarkup();
         List<KeyboardRow> keyboard = new ArrayList<>();
@@ -67,7 +73,7 @@ public class CalculatorBot extends TelegramLongPollingBot {
         }
     }
 
-    public void sendText(String text, String chatId) {
+    private void sendText(String text, String chatId) {
         SendMessage sendMessage = new SendMessage(chatId, text);
 
         try {
@@ -75,6 +81,41 @@ public class CalculatorBot extends TelegramLongPollingBot {
         } catch (TelegramApiException e) {
             e.printStackTrace();
         }
+    }
+
+    private String calculate(String text) {
+        BigDecimal result = new BigDecimal("0");
+        char operator = '+';
+        int firstIndent = 0;
+
+        for (int i = 0; i < text.length(); i++) {
+            if (isOperator(text.charAt(i)) || i + 1 == text.length()) {
+                BigDecimal number;
+
+                if (i + 1 == text.length())
+                    number = new BigDecimal(text.substring(firstIndent, i + 1));
+                else
+                    number = new BigDecimal(text.substring(firstIndent, i));
+
+                if (operator == '+')
+                    result = result.add(number);
+                else if (operator == '-')
+                    result = result.subtract(number);
+                else if (operator == '\u00D7')
+                    result = result.multiply(number);
+                else if (operator == '\u00F7')
+                    result = result.divide(number);
+
+                operator = text.charAt(i);
+                firstIndent = i + 1;
+            }
+        }
+
+        return result.toString();
+    }
+
+    private boolean isOperator(char c) {
+        return c == '+' || c == '-' || c == '\u00F7' || c == '\u00D7';
     }
 
     @Override
