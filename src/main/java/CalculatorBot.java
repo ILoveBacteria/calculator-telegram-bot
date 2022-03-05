d74a62d879e0709d670f5713e515b75ceafba04e
Moein Arabi <moein.mo81@gmail.com>
2022-03-05 23:16:31 +0330

    Apache Maven was added to the project thus moved to a new folder

--- src/main/java/CalculatorBot.java
+++ src/main/java/CalculatorBot.java
@@ -6,7 +6,6 @@ import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.Keyboard
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 import java.math.BigDecimal;
-import java.math.BigInteger;
 import java.math.RoundingMode;
 import java.util.ArrayList;
 import java.util.List;
