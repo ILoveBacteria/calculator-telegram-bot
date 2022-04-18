fca8df83f9775b1585a705e51003302a4d9af72f
Moein Arabi <moein.mo81@gmail.com>
2022-04-18 12:53:10 +0430

    Added Command.java

---
+++ src/main/java/Command.java
@@ -0,0 +1,24 @@
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+
+public class Command {
+    private static final String START = "/start";
+
+    public static SendMessage doAction(String command) {
+        if (command == null) {
+            return null;
+        }
+
+        SendMessage sendMessage;
+
+        switch (command) {
+            case START:
+                sendMessage = new SendMessage();
+                sendMessage.setText("Welcome to Calculator bot");
+            default:
+                sendMessage = new SendMessage();
+                sendMessage.setText("Invalid command!");
+        }
+
+        return sendMessage;
+    }
+}
