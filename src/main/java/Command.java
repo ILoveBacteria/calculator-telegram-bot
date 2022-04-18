dfb75dd6d784fd6011a7e58a8bba61d513c50b12
Moein Arabi <moein.mo81@gmail.com>
2022-04-18 13:34:26 +0430

    Added /help Command

--- src/main/java/Command.java
+++ src/main/java/Command.java
@@ -2,18 +2,23 @@ import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 
 public class Command {
     private static final String START = "/start";
+    private static final String HELP = "/help";
 
     public static SendMessage doAction(String command) {
         if (command == null) {
             return null;
         }
 
-        SendMessage sendMessage;
+        SendMessage sendMessage = new SendMessage();
 
         switch (command) {
             case START:
-                sendMessage = new SendMessage();
                 sendMessage.setText("Welcome to Calculator bot");
+                break;
+            case HELP:
+                sendMessage.setText("This calculator performs simple mathematical operations.\n" +
+                        "To input numbers use the custom keyboard.\nDon't try to test this bot, " +
+                        "it is baby and can't handle all exceptions!");
                 break;
             default:
                 sendMessage = new SendMessage();
