aa4794f34e0cd67dc20f5c7c16a6eb50ee535262
Moein Arabi <moein.mo81@gmail.com>
2022-05-05 17:22:44 +0430

    Cunstroctor params changed for CalculatorBot class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,8 @@ import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 
+import java.io.IOException;
+
 /**
  * This class execute the bot
  *
@@ -12,8 +14,8 @@ public class Main {
     public static void main(String[] args) {
         try {
             TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
-            botsApi.registerBot(new CalculatorBot());
-        } catch (TelegramApiException e) {
+            botsApi.registerBot(new CalculatorBot(FileManagement.readUserAndChat()));
+        } catch (TelegramApiException | IOException e) {
             e.printStackTrace();
         }
     }
